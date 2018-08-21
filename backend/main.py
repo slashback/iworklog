f7770df528d0f246923039f3b711fae3e6ddd5aa
Maxim <mazaev.maxim@gmail.com>
2018-08-21 23:46:29 +0300

    use aiohttp

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,15 @@
 import json
-import requests
-from bottle import route, run, response
+from typing import Any, Dict, List
+from aiohttp import web, BasicAuth, ClientSession
 from os import environ
-from requests.auth import HTTPBasicAuth
 
 
 jira_user = environ.get('JIRA_USER')
 jira_token = environ.get('JIRA_TOKEN')
 
-def get_issues():
+async def fetch_issues_from_jira(
+        client: ClientSession
+    ) -> List[Dict[str, Any]]:
     if not (jira_user and jira_token):
         raise EnvironmentError(
             'Please set correct credentials (JIRA_USER, JIRA_TOKEN)'
@@ -17,12 +18,22 @@ def get_issues():
         'https://cianru.atlassian.net/rest/api/2/search?jql='
         'sprint%20in%20openSprints%20()%20%26%20assignee%20%3D%20m.mazaev'
     )
-    response = requests.get(
+    async with client.get(
         get_issues_url,
-        auth=HTTPBasicAuth(jira_user, jira_token),
-    ).json()
+        auth=BasicAuth(jira_user, jira_token)
+    ) as resp:
+        r = await resp.text()
+        assert resp.status == 200
+        jira_resp = await resp.json()
+        return jira_resp['issues']
+
+async def get_issues() -> List[Dict[str, Any]]:
+    raw_issues = []
+    async with ClientSession() as client:
+        raw_issues = await fetch_issues_from_jira(client)
+    
     result = []
-    for issue in response['issues']:
+    for issue in raw_issues:
         resp_item = dict(
             uid=issue['key'],
             issue_type=issue['fields']['issuetype']['name'],
@@ -36,21 +47,23 @@ def get_issues():
     return result
 
 
-@route('/issues')
-def get_current_sprint_issues():
+async def get_current_sprint_issues(request: web.Request) -> web.Response:
     issues = []
     error = None
     try:
-        issues = get_issues()
+        issues = await get_issues()
     except Exception as ex:
         error = str(ex)
     resp = {
         'issues': issues,
         'error': error,
     }
-    response.content_type = 'application/json'
-    return json.dumps(resp)
+    return web.json_response(resp)
 
 
 if __name__ == '__main__':   
-    run(host='localhost', port=8080)
+    app = web.Application()
+    app.add_routes([
+        web.get('/issues', get_current_sprint_issues)
+    ])
+    web.run_app(app)
