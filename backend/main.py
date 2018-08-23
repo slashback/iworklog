a987dc1c79808f7b90f9d3162241270a156aa4df
Maxim <mazaev.maxim@gmail.com>
2018-08-23 22:35:52 +0300

    pass jira user_name in url

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,8 @@ from os import environ
 
 jira_user = environ.get('JIRA_USER')
 jira_token = environ.get('JIRA_TOKEN')
-# jira_user = 'm.mazaev'
-jira_username = 'alexey.sozykin'
 
+routes = web.RouteTableDef()
 
 @dataclass
 class Issue:
@@ -22,7 +21,8 @@ class Issue:
 
 
 async def fetch_issues_from_jira(
-        client: ClientSession
+        client: ClientSession,
+        user_name: str,
     ) -> List[Dict[str, Any]]:
     if not (jira_user and jira_token):
         raise EnvironmentError(
@@ -30,7 +30,7 @@ async def fetch_issues_from_jira(
         )
     get_issues_url = (
         f'https://cianru.atlassian.net/rest/api/2/search?jql='
-        f'sprint%20in%20openSprints%20()%20%26%20assignee%20%3D%20{jira_username}'
+        f'sprint%20in%20openSprints%20()%20%26%20assignee%20%3D%20{user_name}'
     )
     async with client.get(
         get_issues_url,
@@ -40,10 +40,10 @@ async def fetch_issues_from_jira(
         jira_resp = await resp.json()
         return jira_resp['issues']
 
-async def get_issues() -> List[Issue]:
+async def get_issues(user_name: str) -> List[Issue]:
     raw_issues: List[Dict[str, Any]] = []
     async with ClientSession() as client:
-        raw_issues = await fetch_issues_from_jira(client)
+        raw_issues = await fetch_issues_from_jira(client, user_name)
     
     result = []
     for issue in raw_issues:
@@ -58,12 +58,13 @@ async def get_issues() -> List[Issue]:
         result.append(resp_item)
     return result
 
-
+@routes.get('/api/issues/{user_name}')
 async def get_current_sprint_issues(request: web.Request) -> web.Response:
+    user_name = request.match_info['user_name']
     issues: List[Issue] = []
     error = None
     try:
-        issues = await get_issues()
+        issues = await get_issues(user_name)
     except Exception as ex:
         error = str(ex)
     resp = {
@@ -75,7 +76,5 @@ async def get_current_sprint_issues(request: web.Request) -> web.Response:
 
 if __name__ == '__main__':   
     app = web.Application()
-    app.add_routes([
-        web.get('/api/issues', get_current_sprint_issues)
-    ])
+    app.add_routes(routes)
     web.run_app(app)
